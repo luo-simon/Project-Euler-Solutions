53a575fbc11c238b64966cad97070e679ee87c96
Simon <47718189+luo-simon@users.noreply.github.com>
2020-08-14 14:15:42 +0100

    add file

--- 016.cpp
+++ 016.cpp
@@ -3,9 +3,27 @@
 // What is the sum of the digits of the number 21000?
 
 #include <iostream>
+#include "InfInt.h" //https://sercantutar.github.io/infint/
+
 using namespace std;
 
+InfInt power(int base, int exponent);
+
 int main(){
-    
+    InfInt number = power(2, 1000);
+    string numberStr = number.toString();
+    int sum = 0;
+    for (int i = 0; i < numberStr.length(); i++){
+        sum += numberStr[i] - '0';
+    }
+    cout << sum << endl;
     return 0;
+}
+
+InfInt power(int base, int exponent){
+    InfInt result = 1;
+    for (int i = 0; i < exponent; i++){
+        result *= base;
+    }
+    return result;
 }
